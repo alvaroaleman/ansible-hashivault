f110e2e5d7ca54a1483cce5e3c19fc57c2f11cd8
Alvaro Aleman <alv2412@googlemail.com>
2017-08-17 23:08:36 +0200

    Add trim_rule function

--- library/hashivault_policy.py
+++ library/hashivault_policy.py
@@ -74,19 +74,45 @@ def main():
         argument_spec       = dict(
             name            = dict(required=True),
             state           = dict(default='present', choices=['present', 'absent']),
-            content         = dict(type='string'),
+            rules           = dict(type='list'),
             token           = dict(type='string'),
         ),
-    required_if = [ ("state", "present", ["content"])],
+    required_if = [("state", "present", ["rules"])],
     supports_check_mode = True
     )
 
+    policy_name =  module.params['name']
+
     try:
         conn = HashivaultConnection(token=module.params['token'], dry_mode=module.check_mode)
+        rules = conn.make_request('/v1/sys/policy')[0].get('data', {}).get('policies', {})
+
+        policy_present = policy_name in rules
+
+        if policy_present:
+            if module.params['state'] == 'present':
+                rules = conn.make_request('/v1/sys/policy/%s' % policy_name)[0].get('rules', '')
+                rules = clean_rule(rules)
+                module.exit_json(msg=rules)
+            else:
+                conn.make_request('/v1/sys/policy/%s' % policy_name, method='DELETE')
+                module.exit_json(changed=True)
     except Exception as e:
         module.fail_json(msg=e.message)
-    module.exit_json()
 
+
+def clean_rule(rule):
+    """
+    Removes spaces after opening curly brackets and
+    before closing curly brackets
+    """
+    elements = rule.split('{')
+    elements[1] = elements[1].lstrip()
+    rule = '{'.join(elements)
+    elements = rule.split('}')
+    elements[0] = elements[0].rstrip()
+    rule = '}'.join(elements)
+    return rule
 
 if __name__ == '__main__':
     main()
--- library/hashivault_policy_templ.py
+++ library/hashivault_policy_templ.py
@@ -10,21 +10,38 @@ def main():
     supports_check_mode = True
     )
 
+    policy_name =  module.params['name']
+
     try:
         conn = HashivaultConnection(token=module.params['token'], dry_mode=module.check_mode)
         rules = conn.make_request('/v1/sys/policy')[0].get('data', {}).get('policies', {})
 
-        policy_present = module.params['name'] in rules
+        policy_present = policy_name in rules
 
         if policy_present:
-            if module.params['state'] == 'absent':
-                conn.make_request('/v1/sys/policy/%s' % module.params['name'], method='DELETE')
+            if module.params['state'] == 'present':
+                rules = conn.make_request('/v1/sys/policy/%s' % policy_name)[0].get('rules', '')
+                rules = trim_rule(rules)
+                module.exit_json(msg=rules)
+            else:
+                conn.make_request('/v1/sys/policy/%s' % policy_name, method='DELETE')
                 module.exit_json(changed=True)
     except Exception as e:
         module.fail_json(msg=e.message)
 
-    module.exit_json()
 
+def trim_rule(rule):
+    """
+    Removes spaces after opening curly brackets and
+    before closing curly brackets
+    """
+    elements = rule.split('{')
+    elements[1] = elements[1].lstrip()
+    rule = '{'.join(elements)
+    elements = rule.split('}')
+    elements[0] = elements[0].rstrip()
+    rule = '}'.join(elements)
+    return rule
 
 if __name__ == '__main__':
     main()
