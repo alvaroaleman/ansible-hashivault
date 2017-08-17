73b10214a0944db14a1e76276339dd3b914e4cb4
Alvaro Aleman <alv2412@googlemail.com>
2017-08-17 22:42:31 +0200

    Add support for removing policies

--- Makefile
+++ Makefile
@@ -7,10 +7,14 @@ library/hashivault_authbackend.py:
 	@cat library/hashivault_common.py > library/hashivault_authbackend.py
 	@cat library/hashivault_authbackend_templ.py >> library/hashivault_authbackend.py
 
-.PHONY: hashivault_policy.py
-hashivault_policy.py:
+.PHONY: hashivault_policy
+hashivault_policy:
 	@cat library/hashivault_common.py > library/hashivault_policy.py
 	@cat library/hashivault_policy_templ.py >> library/hashivault_policy.py
+
+.PHONY: hashivault_policy-test
+hashivault_policy-test: hashivault_policy
+	ansible-playbook test/playbook.yml -t testsetup,hashivault_policy -vvvv
 
 .PHONY: modules
 modules: library/hashivault_authbackend.py hashivault_policy.py
--- library/hashivault_policy_templ.py
+++ library/hashivault_policy_templ.py
@@ -3,17 +3,26 @@ def main():
         argument_spec       = dict(
             name            = dict(required=True),
             state           = dict(default='present', choices=['present', 'absent']),
-            content         = dict(type='string'),
+            rules           = dict(type='list'),
             token           = dict(type='string'),
         ),
-    required_if = [ ("state", "present", ["content"])],
+    required_if = [("state", "present", ["rules"])],
     supports_check_mode = True
     )
 
     try:
         conn = HashivaultConnection(token=module.params['token'], dry_mode=module.check_mode)
+        rules = conn.make_request('/v1/sys/policy')[0].get('data', {}).get('policies', {})
+
+        policy_present = module.params['name'] in rules
+
+        if policy_present:
+            if module.params['state'] == 'absent':
+                conn.make_request('/v1/sys/policy/%s' % module.params['name'], method='DELETE')
+                module.exit_json(changed=True)
     except Exception as e:
         module.fail_json(msg=e.message)
+
     module.exit_json()
 
 
